f94eea89d8da93c98e051db376e30f7d9563d866
sruizdeleon <ruizdeleonsergio8@gmail.com>
2023-11-14 20:12:51 +0100

    solving the exercise and editing code

--- S8/2.fetch/exercise-1.js
+++ S8/2.fetch/exercise-1.js
@@ -1,4 +1,18 @@
 /* Utiliza esta url de la api Agify 'https://api.agify.io?name=michael' para hacer
 un .fetch() y recibir los datos que devuelve. Imprimelo mediante un console.log(); */
 
-const request = fetch('https://api.agify.io?name=john').then(response => response.json()).then(data => console.log(data)).catch(error => console.error(error));
+const request = () => {fetch('https://api.agify.io?name=john').then(response => response.json()).then(data => console.log(data)).catch(error => console.error(error))};
+
+request();
+
+
+/* Usando AJAX */
+/* request2.send();
+const request2 = new XMLHttpRequest();
+request2.open('GET', 'https://api.agify.io?name=john');
+request2.addEventListener('load', function () {
+    const data2 = JSON.parse(this.responseText);
+    console.log(JSON.stringify(data2));
+})
+
+request2.send(); */
